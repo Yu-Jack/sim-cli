65d55a5fe8e1fdc78e163b370dd582ce6d8b5034
YuJack <jk82421@gmail.com>
2025-12-03 16:33:12 +0800

    change icon
    Signed-off-by: YuJack <jk82421@gmail.com>

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -9,9 +9,15 @@ export const Layout: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex">
-              <Link to="/" className="flex-shrink-0 flex items-center text-indigo-600 font-bold text-xl">
-                <LayoutDashboard className="w-6 h-6 mr-2" />
-                Harvester Diagnostic
+              <Link to="/" className="flex-shrink-0 flex items-center gap-4">
+                <img 
+                  src="https://docs.harvesterhci.io//img/logo_horizontal.svg" 
+                  alt="Harvester Logo" 
+                  className="h-8"
+                />
+                <span className="text-gray-900 font-semibold text-lg border-l pl-4 border-gray-300">
+                  Support Bundle Disagnostic
+                </span>
               </Link>
             </div>
           </div>
